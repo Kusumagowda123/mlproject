7fca17d62afeeafa8a68414a550a46c8a999727d
Kusumagowda123 <kusumagowda799@gmail.com>
2025-12-30 20:10:42 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -13,9 +13,10 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)   ##inheriting the properties of the parent class(init__)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
-        return self.error_message  
-
+        return self.error_message
+ 
+        
