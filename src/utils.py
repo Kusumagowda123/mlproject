60d993ac754b50288d4a37ef8d33aa3c0e274ff2
Kusumagowda123 <kusumagowda799@gmail.com>
2025-12-31 18:42:37 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-import dill  # dill is used for serializing and deserializing python objects    
+import dill  # dill is used for serializing and deserializing python objects 
+from sklearn.metrics import r2_score   
 def save_object(file_path, obj):
     '''
     Save a python object to a file
@@ -17,5 +18,33 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+
+    '''
+    This function evaluates multiple machine learning models and returns their R2 scores.
+    '''
+    try:
+        
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+           
+
+            # Train the model
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+
+            # Predict on test data
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
     except Exception as e:
         raise CustomException(e, sys)
